435d6630895da0639ac9f855d43647fdc5bda64e
proffix4 <proffix4@gmail.com>
2021-04-19 18:35:18 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hibernate_demo_ant
+Пример приложения с Hibernate для IntelliJ IDEA на Java (сборщик ANT)
---
+++ src/kz/proffix4/hibernate/HibernateUtil.java
@@ -0,0 +1,24 @@
+package kz.proffix4.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            sessionFactory = configuration.configure().buildSessionFactory();
+        } catch (ExceptionInInitializerError ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
